faf3177266788f32b46ee6493c09e26120d884fd
Jyotiprasad1996 <90128873+Jyotiprasad1996@users.noreply.github.com>
2021-09-07 11:56:26 +0530

    Add files via upload

---
+++ brume.js
@@ -0,0 +1,16 @@
+$("document").ready(function(){
+$("#loadMMore_control").click(function(){
+$("#loadMMore_view").css("display" , "inline-flex" )
+$("#loadMMore_control").hide();
+});
+});
+
+// $(document).ready(function(){
+  
+//     $("#loadMMore_control").click(function(){
+//       $("#loadMMore_view").show();
+//       $("#loadMMore_control").hide();
+//     });
+//   });
+
+
